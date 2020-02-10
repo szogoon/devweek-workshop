0313fee121efa0e63dc4c74634b21487a7d64052
Przemyslaw Malolepszy <przemyslaw.malolepszy@tomtom.com>
2020-02-10 15:15:20 +0100

    renamed files

--- app_fuzzy.js
+++ app_fuzzy.js
@@ -2,19 +2,11 @@ import apiKey from './config.js';
 
 var SanFrancisco = 'San Francisco, California'
 
-var categories = {
-    Italian: '7315025',
-    Thai: '7315048',
-    French: '7315017'
-}
-
 var map = tt.map({
     key: apiKey,
     container: 'map',
     style: 'tomtom://vector/1/basic-main'
 });
-
-var markers = [];
 map.addControl(new tt.FullscreenControl());
 map.addControl(new tt.NavigationControl());
 
@@ -28,23 +20,15 @@ map.on('load', function() {
     .then(response => moveMapToFirstResult(response))
 })
 
-var buttons = document.querySelectorAll('.searchBtn')
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        markers.forEach((marker) => {
-            marker.remove();
-        })
-        markers = [];
-        tt.services.fuzzySearch({
-            key: apiKey,
-            center: map.getCenter(),
-            query: 'restaurant',
-            categorySet: categories[event.target.value]
-        })
-        .go()
-        .then(response => createMarkersFromSearch(response)
-        )
+document.querySelector('#searchButton').addEventListener('click', function() {
+    tt.services.poiSearch({
+        key: apiKey,
+        center: map.getCenter(),
+        query: document.querySelector('#searchFieldText').value
     })
+    .go()
+    .then(response => createMarkersFromSearch(response)
+    )
 })
 function moveMapToFirstResult(response) {
     map.flyTo(
@@ -55,10 +39,10 @@ function moveMapToFirstResult(response) {
 function createMarkersFromSearch(response) {
     response.results.forEach(result => {
         var popup = new tt.Popup({offset: 30}).setHTML(createPopupContent(result));
-        markers.push(new tt.Marker()
+        new tt.Marker()
                 .setLngLat(result.position)
                 .setPopup(popup)
-                .addTo(map));
+                .addTo(map);
     })
 }
 function createPopupContent(result) {
